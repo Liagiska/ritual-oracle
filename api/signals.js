5bf5680bd125de7459eb96c97111b7986a315dcd
Liagiska <liagiska86@gmail.com>
2026-05-02 11:03:46 +0000

    Fix API proxy path

--- api/signals.js
+++ api/signals.js
@@ -1,7 +1,6 @@
 export default async function handler(req, res) {
   try {
-    // Ganti dengan IP VPS kamu yang asli
-    const VPS_IP = '159.223.221.130'; 
+    const VPS_IP = '159.223.221.130'; // GANTI DENGAN IP VPS KAMU
     const response = await fetch(`http://$159.223.221.130:3000/api/signals/latest`);
     const data = await response.json();
     res.status(200).json(data);
